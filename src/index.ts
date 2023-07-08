00823aca57585b95104a01cf9ace4b5a385bbac5
Raymond Baril <0x25cfb68@gmail.com>
2023-07-08 17:52:20 +0400

    fix: export list utilities

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default as isTokenList } from '$src/validate.js';
+export { combine, download, find, match, parse, toMap } from '$src/list.js';
+export type { List, ListMap, Token, TokenMap } from '$src/list.js';
+export type { TokenInfo, TokenList, Tags } from '$src/schema.js';
