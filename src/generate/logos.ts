8c8e090e0cc25c671b094d29f2fb43d4cae8e5dd
Raymond Baril <0x25cfb68@gmail.com>
2023-08-21 16:54:47 +0400

    feat: add meta generation on build

--- src/generate/logos.ts
+++ src/generate/logos.ts
@@ -36,12 +36,35 @@ const SVGOConfig: SVGOConfig = {
 	]
 };
 
+type Meta = Record<string, { symbol: string; id?: string; color?: string }>;
+
+async function getMeta(folder: string) {
+	try {
+		const content = await fs.readFile(path.resolve(folder, 'meta.json'), 'utf-8');
+		return JSON.parse(content) as Meta;
+	} catch {
+		return {} as Meta;
+	}
+}
+
 async function generate(from: string, to: string, folder: string) {
 	const config = structuredClone(BaseSVGSpriterConfig);
 
 	config.shape = config.shape ?? {};
 
-	config.shape.id = { ...config.shape.id, generator: `token-${folder}-` };
+	const Meta = await getMeta(path.resolve(from, folder));
+
+	const info = {} as Record<string, { logo: string; color: string }>;
+
+	config.shape.id = {
+		...config.shape.id,
+		generator: name => {
+			const meta = Meta[name];
+			const id = meta?.id ?? `token-${folder}-${name.replace('.svg', '')}`;
+			if (meta) info[meta.symbol] = { logo: `#${id}`, color: meta.color ?? '#000' };
+			return id;
+		}
+	};
 
 	const spriter = new SVGSpriter(config);
 	const entries = await fg([`${from}/${folder}/*.svg`]);
@@ -54,11 +77,10 @@ async function generate(from: string, to: string, folder: string) {
 
 	const { result: compiled } = await spriter.compileAsync();
 
-	const file = path.join(to, `${folder}.svg`);
-
 	const { data: optimized } = optimize(compiled.symbol.sprite.contents, SVGOConfig);
 
-	await fs.writeFile(file, optimized);
+	await fs.writeFile(path.join(to, `${folder}.svg`), optimized);
+	if (Object.keys(info).length) await fs.writeFile(path.join(to, `${folder}.json`), JSON.stringify(info, null, 2));
 }
 
 export default async function (from: string, to: string) {
